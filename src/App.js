ecfa8a1e80c0460b9d8fe060077e3641162c3e8a
minju <alswn1583@naver.com>
2023-10-19 09:59:06 +0900

    [Feat] 글로벌 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import GlobalStyles from "./styles/GlobalStyle";
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <GlobalStyles />
+      hello world
+    </div>
+  );
 }
 export default App;
