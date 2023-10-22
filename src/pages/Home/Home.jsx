05868ce3b49f07cb6e2bcecf55182aeca9ee4276
limm1222 <limm1222@naver.com>
2023-10-22 20:25:16 +0900

    [Design] 헤더 CSS

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import HomeNav from "../../components/Header/HomeHeader";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <HomeNav />
+      Home
+    </div>
+  );
 }
