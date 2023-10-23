e2fbfdb645038310cda95e1c76bce603be6defb9
Rami <138556019+rami242424@users.noreply.github.com>
2023-10-24 02:52:10 +0900

    [Feat] button, aligns styled

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import BasicLayout from "./components/Layout/BasicLayout";
 import AppRouter from "./routes/AppRouter";
 import GlobalStyles from "./styles/GlobalStyle";
-// import { useState } from "react";
-// import Profile from "./Components/Profile";
-
-// function App() {
-  // merge conflicts part
-  // const [page, setPage] = useState(true);
-  // const [info, setInfo] = useState("");
-
-  // const handlePage = () => {
-  //   setPage((prev) => !prev);
-  // };
-
-  // return (
-  //   <div>
-  //     {<Profile handlePage={handlePage} /> }
 
 function App() {
   return (
---
+++ src/pages/Profile/ProfileStyle.jsx
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import AddImg from "../../images/img-btn.png";
+import imgSrc from "../../images/Group2323.png";
+
+export const Container = styled.div`
+  margin: 0 auto;
+
+  .spriteImg-wrapper {
+    position: absolute;
+    bottom: 0;
+  }
+
+  .upload-button {
+    position: absolute;
+    left: 0;
+  }
+`;
+
+export const Title = styled.h1`
+  padding-top: 30px;
+  padding-bottom: 24px;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 500;
+`;
+
+export const P = styled.p`
+  color: #767676;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 14px;
+`;
+
+export const Section = styled.div`
+  padding-bottom: 30px;
+`;
+
+
+export const LineContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px; 
+  padding-bottom: 30px;
+`;
+
+// export const Label, Input, ImageInput = styled.
+
+export const ImageSection = styled.section`
+`;
+
+export const Label = styled.label`
+  display: block;
+  position: relative;
+  width: 122px;
+  height: 110px;
+  flex-shrink: 0;
+  margin: 3.5rem auto 5.5rem;
+  background: url(${imgSrc}) no-repeat center / 122px 110px;
+  
+  cursor: pointer;
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 3.6em;
+    height: 3.6rem;
+    background: url(${AddImg}) no-repeat center / 3.6rem 3.6rem;
+    z-index: 2;
+    }
+`
+
+export const Image = styled.img`
+  width: 122px;
+  height: 110px;
+  object-fit: contain;
+  border-radius: 50%;
+`;
+
+export const ImageInput = styled.input`
+  width: 322px;
+  height: 48px;
+  position: absolute;
+  z-index: -1000rem;
+  display: none;
+`;
+
+export const input = styled.input`
+    width: 322px;
+    height: 48px;
+`;
