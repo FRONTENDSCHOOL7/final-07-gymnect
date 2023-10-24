00b83d16ef9a961f12365117e4dd7678aa93cc83
minju <alswn1583@naver.com>
2023-10-24 15:54:00 +0900

    [Feat] 나의 프로필 화면 추가, Post 수정

--- src/pages/Profile/MyProfile.jsx
+++ src/pages/Profile/MyProfile.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Post from "../../components/common/Post/Post";
 import MyProfileUp from "../../components/common/Profile/MyProfileUp";
 import ModalNav from "../../components/Header/ModalHeader";
+import { FlexIconImg, GridIconImg, MainWrap, Wrap } from "./MyProfileStyle";
+import flexIcon from "../../assets/images/icon-flex-on.svg";
+import gridIcon from "../../assets/images/icon-grid-off.svg";
 
 export default function MyProfile() {
   return (
     <>
-    <ModalNav />
+      <ModalNav />
       <MyProfileUp />
-      {/* <Post /> */}
+      <MainWrap>
+        <Wrap>
+          <FlexIconImg src={flexIcon} alt="나열방식"></FlexIconImg>
+          <GridIconImg src={gridIcon} alt="그리드방식"></GridIconImg>
+        </Wrap>
+        <Post />
+      </MainWrap>
     </>
   );
 }
