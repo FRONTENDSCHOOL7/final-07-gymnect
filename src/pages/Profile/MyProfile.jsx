651d93415020745c2e6247c01a7cdf14cdf795c7
limm1222 <limm1222@naver.com>
2023-10-24 15:04:52 +0900

    [Feat] 프로필 화면 헤더 추가

--- src/pages/Profile/MyProfile.jsx
+++ src/pages/Profile/MyProfile.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Post from "../../components/common/Post/Post";
 import MyProfileUp from "../../components/common/Profile/MyProfileUp";
+import ModalNav from "../../components/Header/ModalHeader";
 
 export default function MyProfile() {
   return (
     <>
+    <ModalNav />
       <MyProfileUp />
       {/* <Post /> */}
     </>
