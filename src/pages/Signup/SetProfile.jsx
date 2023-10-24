b62d481e96cba36f6398516eceb23826bc85ab45
Rami <138556019+rami242424@users.noreply.github.com>
2023-10-24 10:03:01 +0900

    [Feat] code 수정 및 사진업로드 완료

--- src/pages/Signup/SetProfile.jsx
+++ src/pages/Signup/SetProfile.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Container, LineContainer, ImageSection, ImageInput, Image, Label, Title, P } from "./ProfileStyle";
+import { Container, LineContainer, ImageSection, ImageInput, Image, Label, Title, P } from "./SetProfileStyle";
 import Button from "../../components/common/Button/ButtonContainer";
 import Input from "../../components/common/Input/Input";
 
@@ -11,27 +11,64 @@ const SetProfile = () => {
   };
 
   const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [accountname, setAccountname] = useState("");
-  const [imgSrc, setImgSrc] = useState(""); // This state will be updated with the URL of the image
+  const [imgSrc, setImgSrc] = useState("");
   const [info, setInfo] = useState("");
 
   useEffect(() => {
-    // Initially setting an empty string for imgSrc
+    
     setImgSrc("");
   }, []);
 
   const submitJoin = () => {
-    // Your submitJoin function
+    const joinData = {
+      user: {
+        username: username,
+        email: email,
+        password: password,
+        accountname: accountname,
+        intro: info,
+        image: imgSrc,
+      },
+    };
+    join(joinData);
   };
 
   const uploadImage = async (imageFile) => {
-    // Your uploadImage function
+    const baseUrl = "https://api.mandarin.weniv.co.kr/";
+    const reqUrl = baseUrl + "image/uploadfile";
+    const form = new FormData();
+    form.append("image", imageFile);
+
+    const res = await fetch(reqUrl, {
+      method: "POST",
+      body: form,
+    });
+
+    const json = await res.json();
+    setImgSrc(baseUrl + json.filename);
   };
 
   const handleChangeImage = (e) => {
     const file = e.target.files[0];
     uploadImage(file);
   };
+
+  const join = async (joinData) => {
+    console.log(joinData);
+    const reqUrl = "https://api.mandarin.weniv.co.kr/user/";
+    const data = await fetch(reqUrl, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(joinData),
+    }).then((res) => res.json());
+    console.log(data);
+  };
+
 
   /* 버튼 활성화 */
   // const handleActivateButton = () => {
@@ -45,18 +82,18 @@ const SetProfile = () => {
         <P>나중에 언제든지 변경할 수 있습니다.</P>
 
         <ImageSection>
-          <LineContainer>
-          <Label src={imgSrc} htmlFor="profileImg">
-              <Image src={imgSrc} alt="" />
-          </Label>
-          <ImageInput
+            <LineContainer>
+            <Label src={imgSrc} htmlFor="profileImg">
+              <Image src={imgSrc} alt=""/>
+            </Label>
+            <ImageInput
               type="file"
               onChange={handleChangeImage}
               id="profileImg"
               name="image"
               accept="image/*"
-          />
-          </LineContainer>
+            />
+            </LineContainer>
         </ImageSection>
 
         <LineContainer>
@@ -70,9 +107,9 @@ const SetProfile = () => {
             name="username"
             placeholder="2~10자 이내여야 합니다."
           />
-        {/* </LineContainer> */}
+        </LineContainer>
 
-        {/* <LineContainer> */}
+        <LineContainer>
           <label htmlFor="userIdInput">계정 ID</label>
           <input
             value={accountname}
@@ -82,9 +119,9 @@ const SetProfile = () => {
             name="accountname"
             placeholder="영문, 숫자, 특수문자(,), (_)만 사용 가능합니다."
           />
-        {/* </LineContainer> */}
+        </LineContainer>
 
-        {/* <LineContainer> */}
+        <LineContainer>
           <label htmlFor="userIntroInput">소개</label>
           <input
             type="text"
@@ -98,6 +135,7 @@ const SetProfile = () => {
         <Button width="322px" type="submit" onClick={submitJoin}>
           짐넥 시작하기
         </Button>
+
       </section>
     </Container>
   );
