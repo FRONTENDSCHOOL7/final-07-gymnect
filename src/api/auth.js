70195b07e53d28683a75b62bacb148f8f3d99a14
kwonup <kkwon1122@gmail.com>
2023-10-24 10:04:26 +0900

    commit

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,5 +1,16 @@
 import { instance, imgInstance } from "./axiosInstance";
 
+/* 로그인 */
+export const postUserLogin = async (email, password) => {
+  const loginData = {
+    user: {
+      email: email,
+      password: password,
+    },
+  };
+  const response = await instance.post('/user/login', loginData);
+  return response.data;
+};
 /* 이메일 중복 검사 */
 export const postEmailDuplicate = async (email) => {
   const emailData = {
