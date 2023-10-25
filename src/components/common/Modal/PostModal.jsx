9fcc41884b00af56ad08a849a445226c27cd2f86
Rami <138556019+rami242424@users.noreply.github.com>
2023-10-25 15:24:34 +0900

    [Feat] PostModal 생성

---
+++ src/components/common/Modal/PostModal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Background, Section, Container, Button } from './PostModalStyle';
+
+export default function Modal() {
+
+
+
+  return (
+    <>
+        <Background />
+        <Section>
+            <Container>
+              <Button>설정 및 개인정보</Button>
+              <Button>로그아웃</Button>
+            </Container>
+        </Section>
+    </>
+  )
+}
--- src/pages/Posting/Posting.jsx
+++ src/pages/Posting/Posting.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import UploadNav from '../../components/Header/UploadHeader'
+// export default function Posting() {
+//   return (
+//     <div>
+//       <UploadNav />
+//       Posting</div>
+//   )
+// }
+
+// 여기서부터는 모달확인을 위한 임시창 //
+import Modal from '../../components/common/Modal/PostModal';
+
 
 export default function Posting() {
   return (
-    <div>
-      <UploadNav />
-      Posting</div>
+    <>모달임둥
+      <Modal /> 
+    </>
   )
 }
+// 여기서부터는 모달확인을 위한 임시창 //
