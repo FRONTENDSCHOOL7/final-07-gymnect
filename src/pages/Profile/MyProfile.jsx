61aa9f2be34baf7e344b273386c068729ea1e0ec
Rami <138556019+rami242424@users.noreply.github.com>
2023-10-29 16:27:45 +0900

    MyProfile.jsc 수정

--- src/pages/Profile/MyProfile.jsx
+++ src/pages/Profile/MyProfile.jsx
@@ -1,23 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "../../components/common/Post/Post";
 import MyProfileUp from "../../components/common/Profile/MyProfileUp";
 import ModalNav from "../../components/Header/ModalHeader";
-import { FlexIconImg, GridIconImg, MainWrap, Wrap } from "./MyProfileStyle";
-import flexIcon from "../../assets/images/icon-flex-on.svg";
-import gridIcon from "../../assets/images/icon-grid-off.svg";
+import {
+  FlexIconImg, GridIconImg, MainWrap, Wrap, GridContainer, GridItem, SVGIcon 
+} from "./MyProfileStyle";
+import flexIconOn from "../../assets/images/icon-flex-on.svg";
+import flexIconOff from '../../assets/images/icon-flex-off.svg';
+import gridIconOn from '../../assets/images/icon-grid-on.svg';
+import gridIconOff from "../../assets/images/icon-grid-off.svg";
+import layer from "../../assets/images/icon-img-layers.svg";
 
 export default function MyProfile() {
+  const [isExpandedView, setIsExpandedView] = useState(false);
+
+ 
+  const handleIconClick = (viewType) => {
+    if (viewType === "grid") {
+        setIsExpandedView(true);
+    } else if (viewType === "flex") {
+        setIsExpandedView(false);
+    }
+  };
+
   return (
     <>
       <ModalNav />
       <MyProfileUp />
       <MainWrap>
         <Wrap>
-          <FlexIconImg src={flexIcon} alt="나열방식"></FlexIconImg>
-          <GridIconImg src={gridIcon} alt="그리드방식"></GridIconImg>
+          <FlexIconImg 
+              src={!isExpandedView ? flexIconOn : flexIconOff}
+              alt="나열방식" 
+              onClick={() => handleIconClick('flex')}
+          />
+          <GridIconImg 
+              src={isExpandedView ? gridIconOn : gridIconOff} 
+              alt="그리드방식" 
+              onClick={() => handleIconClick('grid')} 
+          />
         </Wrap>
-        <Post /> 
-        {/* post를 생략하고 사진 9개 그냥 그리드로  */}
+        {isExpandedView ? (
+          <GridContainer isExpanded={true}>
+            <GridItem />
+            <GridItem>
+              <SVGIcon src={layer} alt="SVG Icon" />
+            </GridItem>
+            <GridItem />
+            <GridItem />
+            <GridItem />
+            <GridItem>
+              <SVGIcon src={layer} alt="SVG Icon" />
+            </GridItem>
+            <GridItem />
+            <GridItem />
+            <GridItem>
+              <SVGIcon src={layer} alt="SVG Icon" />
+            </GridItem>
+          </GridContainer>
+        ) : (
+          <Post />
+        )}
       </MainWrap>
     </>
   );
