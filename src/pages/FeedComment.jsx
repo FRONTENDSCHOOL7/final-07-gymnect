32b47cd643e03eeed61a524bf1537a61e5c4784f
limm1222 <limm1222@naver.com>
2023-10-31 20:52:50 +0900

    [Feat] 댓글 기능 추가

--- src/pages/FeedComment.jsx
+++ src/pages/FeedComment.jsx
@@ -1,6 +1,10 @@
-import React from "react";
-import BasicProfile from "../assets/images/signup-profile.svg";
+import React, { useState } from "react";
 import iconDot from "../assets/images/icon-dot.svg";
+import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import DeleteCommentModal from "../components/common/Modal/DeleteCommentModal";
+import ReportModal from "../components/common/Modal/ReportModal";
+import { userInfoAtom } from "../atoms/UserAtom";
 import {
   Container,
   Image,
@@ -12,23 +16,48 @@ import {
   Button
 } from "./FeedCommentStyle";
 
-export default function FeedComment({ comment, onDelete }) {
+export default function FeedComment({ data, setCommentId }) {
+  const userInfo = useRecoilValue(userInfoAtom);
+  const { author, createdAt, content } = data;
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isReportModalOpen, setIsReportModalOpen] = useState(false);
+
+  // 내 아이디면 삭제, 다른 사람 아이디면 신고
+  const handleClickMoreButton = () => {
+    setCommentId(data.id);
+    if (userInfo._id === author._id) {
+      setIsDeleteModalOpen(true);
+    } else {
+      setIsReportModalOpen(true);
+    }
+  };
+
+  const handleCloseDeleteModal = () => setIsDeleteModalOpen(false);
+  const handleCloseReportModal = () => setIsReportModalOpen(false);
+
   return (
     <>
       <Container>
         <CommentSection>
-          <Image src={BasicProfile} alt="유저 프로필 이미지" />
+          <Link to={`/profile/${userInfo.accountname}`}>
+            <Image src={userInfo.profileImg} alt="유저 프로필 이미지" />
+          </Link>
           <Contents>
             <UserInfo>
-              <UserName>{comment.userName}</UserName>
+              <UserName>{userInfo.username}</UserName>
             </UserInfo>
-            <Comment>{comment.text}</Comment>
+            <Comment>{content}</Comment>
           </Contents>
         </CommentSection>
-        <Button onClick={onDelete}>
+        <Button onClick={handleClickMoreButton}>
           <img src={iconDot} alt="삭제" />
         </Button>
       </Container>
+
+      {isDeleteModalOpen && (
+        <DeleteCommentModal onClose={handleCloseDeleteModal} />
+      )}
+      {isReportModalOpen && <ReportModal onClose={handleCloseReportModal} />}
     </>
   );
 }
