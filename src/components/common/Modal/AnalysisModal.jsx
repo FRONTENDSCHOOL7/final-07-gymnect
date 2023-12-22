d89cd214f82498ef4a05f485d4c365c3fa7018fe
kwonup <kkwon1122@gmail.com>
2023-12-22 10:30:04 +0900

    운동데이터추출

--- src/components/common/Modal/AnalysisModal.jsx
+++ src/components/common/Modal/AnalysisModal.jsx
@@ -18,6 +18,7 @@ import {
 } from "./AnalysisModalStyle";
 import { ReactComponent as IconExit } from "../../../assets/images/icon-exit.svg";
 import ChartImg from "../../../assets/images/chart-analysis.png";
+import moment from "moment"; // moment 라이브러리 추가
 
 const AnalysisData = ({ title, value, unit }) => {
   return (
@@ -41,6 +42,7 @@ export default function AnalysisModal({
   let totalVolume = 0;
   let totalDistance = 0;
   let totalTime = 0;
+  let totalKcal = 0;
 
   exerciseData.forEach((data) => {
     if (data.weightSum) {
@@ -50,10 +52,22 @@ export default function AnalysisModal({
       totalDistance += parseFloat(data.distance);
     }
     if (data.time) {
-      totalTime += parseFloat(data.time);
+      totalTime += data.time;
+    }
+    if (data.kcal) {
+      totalKcal += data.kcal;
     }
   });
-  console.log("totalVolume = ", totalVolume);
+  console.log("totalVolume = ", totalVolume); //60으로 나눈 몫
+  console.log("totalTime = ", totalTime); //60으로 나눈나머지
+
+  const hours = Math.floor(totalTime / 60); //
+  const minutes = totalTime % 60;
+
+  // 이번 주의 시작일(일요일)과 종료일(토요일) 계산
+  const startOfWeek = moment().startOf("week").format("YYYY년 MM월 DD일");
+  const endOfWeek = moment().endOf("week").format("DD일");
+
   return isOpen ? (
     <Overlay>
       <ModalWrapper>
@@ -62,7 +76,9 @@ export default function AnalysisModal({
         </CloseButton>
         <PrimaryText>GYM-NECT</PrimaryText>
         <P>이번 주 운동분석</P>
-        <SecondaryText>2023년 11월 5일~11일</SecondaryText>
+        <SecondaryText>
+          {startOfWeek} ~ {endOfWeek}
+        </SecondaryText>
         <StatsContainer>
           <AnalysisData title="볼륨" value={totalVolume} unit="kg" />
           <AnalysisData
@@ -70,14 +86,18 @@ export default function AnalysisModal({
             value={totalDistance.toFixed(2)}
             unit="km"
           />
-          <AnalysisData title="시간" value={totalTime} unit="" />
-          <AnalysisData title="소비 칼로리" value="3577" unit="kcal" />
+          <AnalysisData title="시간" value={hours + ":" + minutes} unit="" />
+          <AnalysisData
+            title="예상 소비 칼로리"
+            value={Math.floor(totalKcal)}
+            unit="kcal"
+          />
         </StatsContainer>
         <ChartWrapper>
           <Chart src={ChartImg} alt="Chart" />
         </ChartWrapper>
         <Description>
-          {username} 님의 이번 주 총 운동시간은 8시간 25분 입니다!
+          {username} 님의 이번 주 총 운동시간은 {hours}시간 {minutes}분 입니다!
         </Description>
       </ModalWrapper>
     </Overlay>
